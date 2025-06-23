3ad3b4069ee7faa27ddcbde6eec2e7db6340b3dd
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-06-23 17:03:13 -0400

    trivial representation

--- Representations/Basic.lean
+++ Representations/Basic.lean
@@ -1,5 +1,33 @@
 
 import Mathlib
 
-def Representation (G : Type*) [Monoid G] (R : Type*) [Semiring R] (V : Type*) [AddCommMonoid V] [Module R V] :=
-  G →* (V ≃ₗ[R] V)
+#print Representation
+
+#check DihedralGroup
+
+-- Let's get the trivial representation
+example (n : ℕ) : Representation ℂ (DihedralGroup n) ℂ :=
+  Representation.trivial _ _ _
+
+-- Let's get the trivial representation over the integers
+example (n : ℕ) : Representation ℤ (DihedralGroup n) ℤ :=
+  Representation.trivial _ _ _
+
+
+#check Equiv.Perm
+
+#check Matrix.GeneralLinearGroup
+#check Matrix.SpecialLinearGroup
+
+#check PresentedGroup
+
+#check QuaternionGroup
+
+#check RegularWreathProduct
+
+#check SemidirectProduct
+#check Matrix.symplecticGroup
+#check pinGroup
+#check spinGroup
+
+
