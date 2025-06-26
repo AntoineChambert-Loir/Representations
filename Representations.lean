81b81d4828d8bcc5d491aefc0f0e963c618404a3
Raghuram Sundararajan <102684766+raghuram-13@users.noreply.github.com>
2025-06-26 14:37:30 -0400

    Import 'Representations.DihedralGroup' in 'Representations.lean'.

--- Representations.lean
+++ Representations.lean
@@ -1,3 +1,1 @@
--- This module serves as the root of the `Representations` library.
--- Import modules here that should be built as part of the library.
-import Representations.Basic
+import Representations.DihedralGroup
